06912062cee76fb10ddf3e73442a6bd910b66bef
= <=>
2020-10-07 10:53:42 +0700

    list produk kategori

--- app/Http/Controllers/ProdukController.php
+++ app/Http/Controllers/ProdukController.php
@@ -5,18 +5,34 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Laptop_komputer;
 use App\Kategoriproduk;
+use App\Hardwaresoftware;
 use App\Brand;
 class ProdukController extends Controller
 {
+    public function slugGetProduk($slug,$model,$tabel){
+     
+        $idktg='';
+        
+        $ktg = Kategoriproduk::where('slug',$slug)->get();
+        foreach ($ktg as $value) {
+            $idktg=$value->id;         
+            $data=$model::where('kategoriproduks_id',$idktg)->get();                
+        }
+        //dd($data);
+        
+        return $data;
+    }
     public function detailProduk($slug){
        //return "Test Produk Detail";
        //contoh laptop-2 , laptop index 0 , 2 itu index 1 dengan metode explode separator -
+        $brand = Brand::all();
+
         $url = explode("-",$slug);
         $tabel = $url[0]; //1
         $id = $url[1];
         $ktg = Kategoriproduk::all();
         $kategori = Kategoriproduk::all();
-        $brand = Brand::all();
+        
         if($tabel=='laptop'){
             $relatedprodukid='';
             $produk=Laptop_komputer::find($id);
@@ -33,10 +49,18 @@ class ProdukController extends Controller
         }
         return view('product.detail_product',['produk'=>$produk,'ktg'=>$ktg,'brand'=>$brand,'related'=>$related,'kategori'=>$kategori]);
     }
-    public function listProduk($kategori=null){
+    public function listProduk($slug=null){
         $ktg = Kategoriproduk::all();
-        $kategori = Kategoriproduk::find($kategori);
-        return view('product.list_product',['ktg'=>$ktg]);
+        $kategori = Kategoriproduk::where('slug',$slug)->get();
+        if($slug=='hardware'){
+            $produk = $this->slugGetProduk($slug,"App\Hardwaresoftware",'hardwaresoftwares');
+        }else{
+            //default kategori produk laptop 
+            $produk = $this->slugGetProduk('laptop',"App\Laptop_komputer",'laptop_komputers');
+        }
+        
+        //return $produk;
+       return view('product.list_product',['ktg'=>$ktg,'kategori'=>$kategori,'produk'=>$produk]);
     }
 
 }
